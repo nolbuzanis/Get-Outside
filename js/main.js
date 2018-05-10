8a07f01e6cd4f664c9db1ed2871e50ef074edd7f
nolbuzanis <nolanbuzanis@gmail.com>
2018-05-10 14:43:57 -0400

    Created popup window when marker is clicked

--- js/main.js
+++ js/main.js
@@ -4,14 +4,26 @@ function initMap() {
         lat: 43.6532, 
         lng: -79.3832
     };
+    // Map Options
     var options = {
         zoom: 10,
-        center: location
+        center: location,
+        mapTypeId: 'terrain'
     };
+    // Create the map
     var map = new google.maps.Map(document.getElementById('map'), options);
-
-    var marker1 = new google.maps.Marker({
+    // Add marker
+    var marker = new google.maps.Marker({
         position: location, 
-        map: map
+        map: map,
+        icon: ''
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: 'Currrent Location'
+    });
+
+    marker.addListener('click', function(){
+        infoWindow.open(map, marker);
     });
 }
