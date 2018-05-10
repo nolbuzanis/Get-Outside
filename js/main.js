1cde6c484a17a1b75376d2853ee0719faa1de46e
nolbuzanis <nolanbuzanis@gmail.com>
2018-05-10 16:36:15 -0400

    Added Toronto as default location for maps if user location is null

--- js/main.js
+++ js/main.js
@@ -18,13 +18,17 @@ var wanlessPark = {
     lat: 43.7289649,
     lng: -79.3919565
 };
+var toronto = {
+    lat: 43.6532,
+    lng: -79.3832
+};
 
 function initMap() {
     
     // Map Options
     var options = {
-        zoom: 16,
-        center: userLocation,
+        zoom: 15,
+        center: (userLocation) ? userLocation : toronto,
         mapTypeId: 'terrain'
     };
     
