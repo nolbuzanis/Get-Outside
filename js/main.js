d39e22785c247d98c12d12c7389b5062b9ba1103
nolbuzanis <nolanbuzanis@gmail.com>
2018-05-10 15:57:16 -0400

    Centered map around usser's location

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
+var userLocation;
+
+function getUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            console.log(userLocation);
+        });
+    } else {
+        console.log("User location not avaliable");
+    }
+}
+
 var wanlessPark = {
     lat: 43.7289649,
     lng: -79.3919565
 };
 
 function initMap() {
-    var location = {
-        lat: 43.6532, 
-        lng: -79.3832
-    };
+    
     // Map Options
     var options = {
-        zoom: 10,
-        center: location,
+        zoom: 16,
+        center: userLocation,
         mapTypeId: 'terrain'
     };
+    
     // Create the map
     var map = new google.maps.Map(document.getElementById('map'), options);
+    
     /*
     // Add marker
     var marker = new google.maps.Marker({
@@ -40,6 +55,6 @@ function initMap() {
             });
         }
 
-        addMarker(wanlessPark);
+        addMarker(userLocation);
 }
 
