1325eb1debde186387e884fd7dac76542ced63c6
nolbuzanis <nolanbuzanis@gmail.com>
2018-05-11 14:43:53 -0400

    Form validation for distance input value. Works with button pressed and enter key.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// Once button is pressed, show the map and results
-
+// Slide to show the map and results, hides the main page
 function slide() {
     var map = document.getElementById('map');
     var page = document.getElementById('home');
@@ -9,9 +8,29 @@ function slide() {
     back.classList.toggle('innactive');
 }
 
-document.getElementById('search').addEventListener('click', function(){
-    slide();
-    initMap();
+// Check to see if value is valid, a number, and greater than 0
+function distanceValidation() {
+    var value = document.getElementById('distance').value;
+    if (!value || isNaN(value)) {
+        console.log('No distance specified.');
+    } else if (value == 0){
+        console.log('Specify a value greater than zero.');
+    }
+    else {
+        slide();
+        initMap();
+        console.log(value + ' km entered');
+    }
+}
+
+// When button is pressed, call distanceValidation function
+document.getElementById('search').addEventListener('click', distanceValidation);
+
+// Call distanceValidation function is enter key is pressed
+document.getElementById('distance').addEventListener('keypress', function(e) {
+    if (e.keyCode == 13) {
+        distanceValidation();
+    }
 });
 
 document.getElementById('back').addEventListener('click', function() {
